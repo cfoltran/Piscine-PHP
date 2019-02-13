eaa0213725fda0f2b822701d27e70adbf4d7efcc
clementfoltran <clfoltra@student.42.fr>
2019-02-13 21:57:10 +0100

    d00 & start of d01

---
+++ d01/ex02/oddeven.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+
+<?php
+    while (true)
+    {
+        echo 'Entrez un nombre: ';
+        $param = trim(fgets(STDIN));
+        if (feof(STDIN) == true)
+            exit();
+        if (is_numeric($param))
+        {
+            if (substr($param, -1) % 2 == 0)
+                echo "Le chiffre ".$param." est Pair\n";
+            else
+                echo "Le chiffre ".$param." est Impair\n";
+        }
+        else
+            echo "'".$param."' n'est pas un chiffre\n";
+    }
+?>
