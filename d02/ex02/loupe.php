e26528a04d655a782f921c176b3256df8c012545
clementfoltran <clfoltra@student.42.fr>
2019-03-18 10:01:20 +0100

    add ex02 to 03

--- d02/ex02/loupe.php
+++ d02/ex02/loupe.php
@@ -1,7 +1,20 @@
 #!/usr/bin/php
 <?php
-
-
-
-
+    if ($argc != 2 && !file_exists($argv[1]))
+        exit();
+    if (!($fd = fopen($argv[1], 'r')))
+        exit();
+    while ($fd && !feof($fd))
+        $html .= fgets($fd);
+    if (!fclose($fd))
+        exit();
+    $html = preg_replace_callback('/title="(.*?)"/', function ($matches) {
+		return  'title="' . strtoupper($matches[1]) .'"';  
+    }, $html);
+    $html = preg_replace_callback('/<a [^>]+.*<\/a>/', function ($matches) {
+        return preg_replace_callback('/>.*</siU', function ($matches) {
+            return strtoupper($matches[0]);
+        }, $matches[0]);
+    }, $html);
+	echo $html;
 ?>
