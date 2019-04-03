3a46fb2baf32ef129d2d1229b620b55710330c80
clementfoltran <clfoltra@student.42.fr>
2019-04-03 15:56:23 +0200

    ex00-05 d07

---
+++ d07/ex06/UnholyFactory.class.php
@@ -0,0 +1,15 @@
+<?php
+    class UnholyFactory {
+        public function absorb(CrippledSoldier $cs) {
+
+        }
+
+        public function fabricate($requested_figther) {
+
+        }
+
+        public function fight($target) {
+            
+        }
+    }
+?>
